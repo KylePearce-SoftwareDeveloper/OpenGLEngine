7187031e5cc45d6db87c21b54d0523848f438f28
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-07-18 16:24:17 +0100

    #10 - Vertex Arrays

--- OpenGL/src/cpp/main.cpp
+++ OpenGL/src/cpp/main.cpp
@@ -112,6 +112,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -142,10 +146,15 @@ int main(void)
         2, 3, 0
     };
 
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
     unsigned int buffer;//will later be the ID of the buffer
     GLCall(glGenBuffers(1, &buffer));//created buffer (section of memory on GPU)
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));//about to work on buffer
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));//fill buffer with data
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));//fill buffer with data
+
     GLCall(glEnableVertexAttribArray(0));//enable the following atrribute
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));//define the layout of the data now in the buffer
 
@@ -162,6 +171,11 @@ int main(void)
     ASSERT(location != -1);
     //GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -170,7 +184,12 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));//draws using currently bound buffer (glBindBuffer) and the data that is inside it
 
         if (r > 1.0f)
